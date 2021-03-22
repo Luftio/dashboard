17c4b462fee198d56ea2cab8a60e61eaa6cc0bba
Tomáš Martykán <t.martykan@gmail.com>
2021-03-22 09:15:35 +0100

    Types

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 type Props = {};
 
 class Document extends NextDocument<Props> {
   // From https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await NextDocument.getInitialProps(ctx);
